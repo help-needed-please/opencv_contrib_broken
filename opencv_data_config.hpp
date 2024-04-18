368c878ad3b891101520de26cd881b57501e30cc
help-needed-please <167385419+help-needed-please@users.noreply.github.com>
2024-04-17 20:24:39 -0500

    Add files via upload

---
+++ opencv_data_config.hpp
@@ -0,0 +1,11 @@
+
+#define OPENCV_INSTALL_PREFIX "D:/testDownloads/opencv Lib/install"
+
+#define OPENCV_DATA_INSTALL_PATH "etc"
+
+#define OPENCV_BUILD_DIR "D:/testDownloads/opencv Lib"
+
+#define OPENCV_DATA_BUILD_DIR_SEARCH_PATHS \
+    "../opencv/"
+
+#define OPENCV_INSTALL_DATA_DIR_RELATIVE "../../../etc"
